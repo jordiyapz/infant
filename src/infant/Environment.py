432e249799d717a78392a91e8b7f6c404cf6311d
Jordi Yaputra <jordiyaputra@gmail.com>
2021-11-07 22:53:32 +0700

    feat: obtain can 9

--- src/infant/Environment.py
+++ src/infant/Environment.py
@@ -3,6 +3,22 @@ import requests
 import torch
 import urllib
 from torchvision.io import decode_png
+from promise import Promise
+from time import sleep
+import asyncio
+from asyncio import Future
+
+def data_uri_to_state(data_uri):
+    '''Convert data_uri to state.'''
+    res = urllib.request.urlopen(data_uri)
+    raw_image:bytes = res.file.read()
+    tensor = torch.frombuffer(raw_image, dtype=torch.uint8)
+    state = decode_png(tensor)[:3]  # iqnore last alpha channel as it always full
+
+    return state
+
+def post_process_action(action):
+    return dict(zip(('x', 'y', 'down'), map(float, action)))
 
 class SimNs(socketio.ClientNamespace):
     def on_connect(self):
@@ -14,9 +30,14 @@ class Environment:
     def __init__(self, sim_id:str=None, base_url='http://localhost:3000'):
         self.base_url = base_url
         self.sim_id = sim_id
-        self._is_stop = False
+
+        self._is_done = False
         self._created_sims = []
         self._on_stop = None
+        self._use_step = False
+        self._recv_render = {'is_new': False, 'data': None}
+        self._init_sent = False
+        self._running_loop = None
 
         self._sock = socketio.Client()
         self.wait = self._sock.wait
@@ -26,10 +47,15 @@ class Environment:
 
         self._episode_ct = 0 
         self._max_episodes = None
+
+        self.prev_state = None
     
     def _register_namespace(self):
         self._ns = SimNs(f'/sim-{self.sim_id}')
         self._sock.register_namespace(self._ns)
+
+    def set_running_loop(self, loop:asyncio.AbstractEventLoop):
+        self._running_loop = loop
 
     def create(self):
         '''Create new simulation'''
@@ -38,24 +64,64 @@ class Environment:
         self._created_sims.append(self.sim_id)
         self._register_namespace()
     
-    def connect(self, close_on_stop=True):
+    def connect(self, disconnect_on_done=True, use_step=True):
         '''Connect to socket.io server'''
         self._sock.on('connect', lambda: print(f'Connected to {self._ns.namespace}'))
         self._sock.connect(self.base_url)
 
-        if close_on_stop:
-            self._on_stop = lambda: self.close()
+        if disconnect_on_done:
+            self._on_stop = lambda: self.disconnect()
+        
+        self._use_step = use_step
+        # if use_step:
+        #     def on_render(*args):
+        #         self._recv_render['is_new'] = True
+        #         self._recv_render['data'] = args
+        #     self._sock.on('sim:render', on_render, self._ns.namespace)
 
     def init(self, max_episodes=None):
         '''Send initialization signal to simulation server. 
         In other words, init simulation-loop.'''
-        self._is_stop = False
+        self._is_done = False
         self._max_episodes = max_episodes
         self._sock.emit('sim:init', namespace=self._ns.namespace)
+        self._init_sent = True
 
-    def close(self):
+    def disconnect(self):
         '''Close socket.io connection'''
         self._sock.disconnect()
+
+    def reset_episode_count(self):
+        self._episode_ct = 0
+    
+    def reset(self):
+        self.reset_episode_count()
+        self.prev_state = None
+
+    def step(self, action:torch.Tensor):
+        '''[WIP] Step through the simulation. 
+        Returns Awaitables that resolves into state.
+        '''
+        if not self._running_loop:
+            raise Exception('Attach event loop first')
+
+        future = self._running_loop.create_future()
+        # try:
+            # while not self._recv_render['is_new']:
+            #     sleep(.01)
+        self._sock.on('sim:render', lambda *args: future.set_result(args), self._ns.namespace)
+        if not self._init_sent:
+            self.init()
+
+        action_dict = dict(zip(('x', 'y', 'down'), map(float, action)))
+        self._sock.emit('sim:action', action_dict, self._ns.namespace)
+            # resolve(self._recv_render['data'])
+        # except Exception as error:
+        #     reject(error)
+
+        return future
+
+        # return Promise(get_next)
 
     def on_state(self, on_state):
         '''Attach simulation-loop
@@ -66,34 +132,30 @@ class Environment:
         '''
         
         def on_render(data_uri, reward):
-            '''Thin wrapper to decode data_uri and send encoded actions.'''
+            '''Thin wrapper to decode data_uri and send encoded action.'''
             try:
-                # Convert data_uri to state
-                res = urllib.request.urlopen(data_uri)
-                raw_image:bytes = res.file.read()
-                tensor = torch.frombuffer(raw_image, dtype=torch.uint8)
-                state = decode_png(tensor)[:3]  # iqnore last alpha channel as it always full
+                state = data_uri_to_state(data_uri)
 
                 # Calls user-defined on_state function
-                actions:torch.Tensor = on_state(state, reward, self)
+                action:torch.Tensor = on_state(state, reward, self)
 
                 # Increment episode count
                 self._episode_ct += 1
                 if (self._episode_ct >= self._max_episodes):
-                    self._is_stop = True
+                    self._is_done = True
 
                 # Intercept the loop and stop it
-                if self._is_stop:
+                if self._is_done:
                     self.stop()
                     return None
 
                 # Here goes the loop
-                action_dict = dict(zip(('x', 'y', 'down'), map(float, actions)))
-                self._sock.emit('sim:action', action_dict, self._ns.namespace)
+                action = post_process_action(action)
+                self._sock.emit('sim:action', action, self._ns.namespace)
 
             except Exception as e:
                 print(f'An exception occured: {e}')
-                self._is_stop = True
+                self._is_done = True
                 return None
 
         self._sock.on('sim:render', on_render, self._ns.namespace)
@@ -104,7 +166,7 @@ class Environment:
     
     def stop(self, stop=True):
         '''Set stop flag to stop simulation-loop'''
-        self._is_stop = stop
+        self._is_done = stop
         if self._on_stop:
             self._on_stop()
 
